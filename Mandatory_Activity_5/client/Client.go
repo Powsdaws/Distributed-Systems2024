a3ba07d5231a9a7c4464b50e9206c692c92773a4
Marius Holm <mariusholm@hotmail.dk>
2024-11-18 15:48:24 +0100

    Working on Mandatory activity 5
    Handin 5
    
    Co-authored-by: Jonathan Antoine Villeret <jonathan.villeret@gmail.com>
    Co-authored-by: VernesNemo <VernesNemo@users.noreply.github.com>

---
+++ Mandatory_Activity_5/client/Client.go
@@ -0,0 +1,134 @@
+package main
+
+import (
+	proto "AuctionSystem/grpc"
+	"bufio"
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+
+	"encoding/json"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+var id uint32
+
+func main() {
+
+	log.Println("Enter an id for the client")
+	fmt.Scanln(&id)
+
+	ConnectServers()
+
+}
+
+var connections []proto.AuctionServiceClient
+
+func ConnectServers() {
+	conn1, err := grpc.NewClient("localhost:5050", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatal(err)
+	}
+	client1 := proto.NewAuctionServiceClient(conn1)
+	conn2, err := grpc.NewClient("localhost:5051", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatal(err)
+	}
+	client2 := proto.NewAuctionServiceClient(conn2)
+	conn3, err := grpc.NewClient("localhost:5052", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatal(err)
+	}
+	client3 := proto.NewAuctionServiceClient(conn3)
+	connections = append(connections, client1)
+	connections = append(connections, client2)
+	connections = append(connections, client3)
+}
+
+func sendBid(amount uint32) {
+	bid := proto.Bid{
+		ClientId: id,
+		Amount:   amount,
+	}
+
+	//We make a bid to every node in the server
+	for _, conn := range connections {
+
+		ack, err := conn.Bid(context.Background(), &bid)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+	}
+}
+
+func sendResult() {
+
+	var results []proto.Outcome
+
+	for _, conn := range connections {
+		result, _ := conn.Result(context.Background(), &proto.Empty{})
+		results = append(results, *result)
+
+		b, _ := json.Marshal(result)
+
+	}
+
+	for _, result := range results {
+
+	}
+}
+
+func getMostOccurring(elements []any) any {
+	var hashes = make(map[string](proto.Outcome))
+	var occurences = make(map[string](int))
+
+	for _, element := range elements {
+		// serialize and hash the result, increment occurences
+		json, _ := json.Marshal(element)
+
+		hashes[string(json)] = element
+		occurences[string(json)] = occurences[string(json)] + 1
+	}
+
+	// get most occuring hash
+	var mostOccuringHash string
+	var mostOccuringOccurence int
+	for hash, occurence := range occurences {
+		if occurence > mostOccuringOccurence {
+			mostOccuringHash = hash
+			mostOccuringOccurence = occurence
+		}
+	}
+
+	return hashes[mostOccuringHash]
+}
+
+func CLI() {
+
+	reader := bufio.NewReader(os.Stdin)
+
+	for {
+		input, err := reader.ReadString('\n')
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		splitInput := strings.Split(input, " ")
+
+		if splitInput[0] == "Bid" {
+			amount, _ := strconv.Atoi(splitInput[1])
+			sendBid(uint32(amount))
+		} else if splitInput[0] == "Result" {
+			sendResult()
+		} else {
+			log.Println("Invalid command")
+		}
+
+	}
+}
---
+++ Mandatory_Activity_5/server/server.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	proto "AuctionSystem/grpc"
+	"context"
+	"errors"
+	"fmt"
+	"log"
+	"net"
+	"time"
+
+	"google.golang.org/grpc"
+)
+
+type AuctionServiceServer struct {
+	proto.UnimplementedAuctionServiceServer
+	EndAt         time.Time
+	HighestBid    uint32
+	HighestBidder uint32
+}
+
+func (s *AuctionServiceServer) Bid(ctx context.Context, bid *proto.Bid) (*proto.Ack, error) {
+
+	//Start Auction if not prior bid has been placed
+	if s.EndAt.IsZero() {
+		s.startAuction()
+	}
+
+	//We check if auction is over
+	currentTime := time.Now()
+	if !currentTime.Before(s.EndAt) {
+		ack := &proto.Ack{
+			Acknowledgement: false,
+		}
+		errorMessage := errors.New("Bid failed: Auction is over")
+		return ack, errorMessage
+	}
+
+	//Check if bid is higher than the current highest bid
+	if bid.Amount > s.HighestBid {
+		s.HighestBid = bid.Amount
+		s.HighestBidder = bid.ClientId
+	}
+
+	ack := &proto.Ack{
+		Acknowledgement: true,
+	}
+
+	return ack, nil
+}
+
+func (s *AuctionServiceServer) Result(ctx context.Context, empty *proto.Empty) (*proto.Outcome, error) {
+	outcome := &proto.Outcome{
+		HighestBid: s.HighestBid,
+	}
+
+	// if auction is over, add the winner
+	outcome.WinnerId = s.HighestBidder
+
+	return outcome, nil
+}
+
+func main() {
+	// get a host port from the user
+
+	log.Println("Declare a host port for this server, e.g.: 5050")
+	var port string
+	fmt.Scanln(&port)
+
+	// start an auction
+	server := AuctionServiceServer{}
+	server.startServer(port)
+}
+
+func (s *AuctionServiceServer) startServer(port string) {
+	grpcServer := grpc.NewServer()
+	listener, err := net.Listen("tcp", ":"+port)
+	if err != nil {
+		log.Fatalf("Did not work")
+	}
+
+	proto.RegisterAuctionServiceServer(grpcServer, s)
+
+	log.Println("A auction server/node has startd")
+
+	err = grpcServer.Serve(listener)
+
+	if err != nil {
+		log.Fatalf("Something failed while starting the server node")
+	}
+}
+
+func (s *AuctionServiceServer) startAuction() {
+	s.EndAt = time.Now().Add(time.Second * 100)
+}
