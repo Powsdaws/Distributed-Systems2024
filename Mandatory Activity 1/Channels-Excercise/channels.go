ee9b9a2cce86096511d9e2db4bb2da29d226de46
Marius Holm <marho@itu.dk>
2025-09-03 16:07:46 +0200

    added channels excercise

---
+++ Mandatory Activity 1/Channels-Excercise/channels.go
@@ -0,0 +1,34 @@
+package main
+
+import "time"
+
+func main() {
+	ch1 := make(chan string)
+	ch2 := make(chan string)
+
+	go pingerA(ch1, ch2)
+	go pingerB(ch1, ch2)
+
+	for { //Loop so the program does not terminate before the goroutines start
+	}
+
+}
+
+func pingerA(ch1 chan string, ch2 chan string) {
+	for {
+		ch1 <- "ping"
+		time.Sleep(100 * time.Millisecond)
+		read := <-ch2
+		println("A reviewed: ", read)
+	}
+
+}
+
+func pingerB(ch1 chan string, ch2 chan string) {
+	for {
+		read := <-ch1
+		println("A reviewed: ", read)
+		time.Sleep(100 * time.Millisecond)
+		ch2 <- "pong"
+	}
+}
